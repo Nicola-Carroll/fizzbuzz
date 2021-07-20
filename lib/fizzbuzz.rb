a5478008c7b25ad228ce64653d819b0f2d61ab36
danroche10 <danielroche10@gmail.com>
2021-07-20 10:56:49 +0100

    first refactor

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,22 @@
+
+
 def fizzbuzz(number)
-  if number % 5 == 0 && number % 3 != 0
-    return 'buzz'
-  elsif number % 3 == 0 && number % 5 != 0
-    return 'fizz'
-  elsif number % 3 == 0 && number % 5 == 0
-    return 'fizzbuzz'
-  else
-    return number
-  end
+
+  outcomes = {
+    [true, false] => "buzz",
+    [false, true] => "fizz",
+    [true, true] => "fizzbuzz"
+  }
+
+  array = [number % 5 == 0, number % 3 == 0]
+  outcomes[array].nil? ? number : outcomes[array]
+  # if number % 5 == 0 && number % 3 != 0
+  #   'buzz'
+  # elsif number % 3 == 0 && number % 5 != 0
+  #   'fizz'
+  # elsif number % 3 == 0 && number % 5 == 0
+  #   'fizzbuzz'
+  # else
+  #   number
+  # end
 end
