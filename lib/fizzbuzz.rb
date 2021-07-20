01ce69f6ee7b8265fcfa55bef4e4c88d5d41f8e0
nicola <nicolaccarroll@gmail.com>
2021-07-20 11:05:57 +0100

    final code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,3 @@
-
-
 def fizzbuzz(number)
 
   outcomes = {
@@ -10,13 +8,5 @@ def fizzbuzz(number)
 
   array = [number % 5 == 0, number % 3 == 0]
   outcomes[array].nil? ? number : outcomes[array]
-  # if number % 5 == 0 && number % 3 != 0
-  #   'buzz'
-  # elsif number % 3 == 0 && number % 5 != 0
-  #   'fizz'
-  # elsif number % 3 == 0 && number % 5 == 0
-  #   'fizzbuzz'
-  # else
-  #   number
-  # end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,21 +4,12 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-end
-
-describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-end
-
-describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
-end
-
-describe 'fizzbuzz' do
   it 'returns "11" when passed 11' do
     expect(fizzbuzz(11)).to eq 11
   end
